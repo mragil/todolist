fc8d0c4a5b2ef8d302fe182bf14f7eb2ed6c1ed1
Muhammad Ragil Trireza Ramadhan <mragiltrirezar@gmail.com>
2021-10-17 00:17:29 +0700

    Disabling LoadDatabase Configuration

--- src/main/java/com/exampletodo/todo/LoadDatabase.java
+++ src/main/java/com/exampletodo/todo/LoadDatabase.java
@@ -7,9 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
